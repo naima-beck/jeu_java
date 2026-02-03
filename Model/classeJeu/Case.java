6dc315780db387ce2ad61cbf8e3d8b671364c6a9
Naïma Beck <149182197+naima-beck@users.noreply.github.com>
2026-02-03 19:53:14 +0100

    Refactor Case class with constructor and methods

--- Model/classeJeu/Case.java
+++ Model/classeJeu/Case.java
@@ -6,34 +6,42 @@ import java.util.*;
 
 public class Case {
 
-    public Case() {
-    }
-
-    public int x;
+	public int x;
     public int y;
     public Element element;
-
-
-
-    public List<Case> getPlusProcheVoisin(Case c) {
-        return null;
-    }
-
-    public void retirerElement() {
-    }
-
     
-    public boolean contientElement() {
-        return false;
+    public Case(int x, int y) {
+    	this.x=x;
+    	this.y=y;
+    	this.element=null;
     }
 
+    public boolean contientElement() { 
+        return this.element !=null; // retourne un booléen
+    }
+    
+    public void retirerElement() {
+    	this.element = null;
+    }
+
+    public void setElement(Element e) {
+        this.element = e; 
+    }
+        
     
     public int distanceVers(Case c) {
-        return 0;
+    	if (c == null) 
+    		return Integer.MAX_VALUE; // Sécurité
+    	
+        return Math.abs(this.x - c.x) + Math.abs(this.y - c.y);
     }
 
     
-    public void getValeur() {
+    public int getValeur() {
+    	if (contientElement()) {
+            return this.element.getValeur(); 
+        }
+        return 0; // Valeur par défaut si pas d'élément
     }
 
 }
